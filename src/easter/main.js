aba1c56af0850abbd688ff112d2787fe6bac0d4e
du <951092973@qq.com>
2017-04-13 14:34:29 +0800

    link to index

--- src/easter/main.js
+++ src/easter/main.js
@@ -5,7 +5,7 @@ import setWX from "js/wxShare";
 
 setWX({
     title: " ",
-    env:"production",
+    env:"develop",
     desc: "O小姐福利，快来领取你的彩蛋",
     shareImg: "http://wx.ooklady.com/imgs/xiaoqing/easer/loading-eag.png",
     shareLink: "/view/wxpages/activitypage/easter/easter.html",
