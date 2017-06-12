b7731100ca75bf85ae0e10ae87971333ce8c9bab
du <951092973@qq.com>
2017-06-12 20:13:14 +0800

    finish xiaoya ui

--- src/xiaoya/main.js
+++ src/xiaoya/main.js
@@ -1,1 +1,10 @@
 import style from './style.scss'
+import swpercss from "scss/swiper3.min.scss"
+import swiper from "js/swiper3.min.js"
+
+var swip = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    initialSlide :4
+});
+
+
