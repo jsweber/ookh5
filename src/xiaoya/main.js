484476d91e7be2d29293370002528576e6027298
du <951092973@qq.com>
2017-06-13 11:37:18 +0800

    add swiper

--- src/xiaoya/main.js
+++ src/xiaoya/main.js
@@ -4,7 +4,22 @@ import swiper from "js/swiper3.min.js"
 
 var swip = new Swiper('.swiper-container', {
     direction: 'vertical',
-    initialSlide :2
+    noSwiping : true,
 });
-
+let nowPage = 0;
+let maxLen = $(".swiper-slide").length;
+//上一张页面
+$(".backBtn").on("click",function(){
+    if(nowPage >0){
+        nowPage--;
+        swip.slideTo(nowPage, 300, false);
+    }
+})
+//下一张页面
+$(".nextBtn").on("click",function(){
+    if(nowPage <maxLen-1){
+        nowPage++;
+        swip.slideTo(nowPage, 300, false);
+    }
+})
 
